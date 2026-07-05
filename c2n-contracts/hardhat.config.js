e0ae05c1be3661462d4f9af4d2da9fe2c0815ab9
hebszy <281818629@qq.com>
2026-07-06 00:26:41 +0800

    更新版本

--- c2n-contracts/hardhat.config.js
+++ c2n-contracts/hardhat.config.js
@@ -7,10 +7,12 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 31337,
+      blockGasLimit: 30000000,
     },
     local: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
+      gas: 8000000,
       // accounts: [process.env.PRIVATE_KEY],
     },
     // sepolia: {
